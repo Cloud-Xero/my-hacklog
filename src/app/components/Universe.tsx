9b66dd0a958dd6f4a4b2e99fd7c201a587cc4ceb
Cloud-Xero <hiropon201511@gmail.com>
2023-08-10 09:32:59 +0900

    style: apply Universe component

--- src/app/components/Universe.tsx
+++ src/app/components/Universe.tsx
@@ -2,20 +2,25 @@
 
 import { ReactNebula } from "@flodlc/nebula";
 
-export const Universe = () => {
+export const Universe = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ReactNebula
-      config={{
-        starsCount: 400,
-        cometFrequence: 10,
-        starsRotationSpeed: 5,
-        nebulasIntensity: 5,
-        bgColor: "rgb(8,8,8)",
-        sunScale: 0,
-        planetsScale: 0,
-        solarSystemOrbite: 0,
-        solarSystemSpeedOrbit: 0,
-      }}
-    />
+    <>
+      <div className="absolute w-full h-full z-m1">
+        <ReactNebula
+          config={{
+            starsCount: 400,
+            cometFrequence: 10,
+            starsRotationSpeed: 5,
+            nebulasIntensity: 5,
+            bgColor: "rgb(8,8,8)",
+            sunScale: 0,
+            planetsScale: 0,
+            solarSystemOrbite: 0,
+            solarSystemSpeedOrbit: 0,
+          }}
+        />
+      </div>
+      <div className="relative z-0">{children}</div>
+    </>
   );
 };
